68ff570707744491506ccb88da54b66d2216e6a9
Matthew McGrath <mjmcgrath8@gmail.com>
2026-02-04 19:57:35 -0500

    start the io layers for the intake, including methods and inputs

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -3,7 +3,8 @@ package frc.robot.subsystems.intake;
 import org.littletonrobotics.junction.AutoLog;
 
 /**
- * The {@code IntakeIO} class provides methods for interacting with the intake motors and updating
+ * The {@code IntakeIO} class provides methods for interacting with the intake
+ * motors and updating
  * the intake inputs.
  *
  * @author Ryan Hefferon
@@ -12,8 +13,18 @@ import org.littletonrobotics.junction.AutoLog;
  * @author Julien Precourt
  */
 public interface IntakeIO {
-  default void updateInputs(IntakeIOInputs inputs) {}
+  default void updateInputs(IntakeIOInputs inputs) {
+  }
 
   @AutoLog
-  public static class IntakeIOInputs {}
+  public static class IntakeIOInputs {
+    double armMotorVelocityRPM = 0.0;
+    double wheelMotorVelocityRPM = 0.0;
+    double armMotorPositionsRotations = 0.0;
+    double wheelMotorPositionRotations = 0.0;
+
+  }
+
+  default void setArmSpeed(double speed){}
+  default void setWheelSpeed(double speed){}
 }
--- src/main/java/frc/robot/subsystems/intake/IntakeIOHardware.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOHardware.java
@@ -1,3 +1,42 @@
 package frc.robot.subsystems.intake;
 
-public class IntakeIOHardware implements IntakeIO {}
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.EncoderConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+public class IntakeIOHardware implements IntakeIO {
+    SparkMax armMotor = new SparkMax(5, MotorType.kBrushless);
+    SparkMax wheelMotor = new SparkMax(6, MotorType.kBrushless);
+    RelativeEncoder armEncoder = armMotor.getEncoder();
+    RelativeEncoder wheelEncoder = wheelMotor.getEncoder();
+    SparkMaxConfig armConfig;
+    SparkMaxConfig wheelConfig;
+
+    public IntakeIOHardware() {
+        armConfig = new SparkMaxConfig();
+        wheelConfig = new SparkMaxConfig();
+        // armMotor.configure(armConfig, null, null);
+        // wheelMotor.configure(armConfig, null, null);
+    }
+
+    @Override
+    public void setArmSpeed(double speed) {
+        armMotor.set(speed);
+    }
+
+    @Override
+    public void setWheelSpeed(double speed) {
+        wheelMotor.set(speed);
+    }
+
+    @Override
+    public void updateInputs(IntakeIOInputs inputs){
+    inputs.armMotorVelocityRPM = armEncoder.getVelocity();
+    inputs.wheelMotorVelocityRPM = wheelEncoder.getVelocity();
+    inputs.armMotorPositionsRotations = armEncoder.getPosition();
+    inputs.wheelMotorPositionRotations = wheelEncoder.getPosition();
+    }
+
+}
